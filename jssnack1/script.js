9303545000c5fd0222a049408fef5375b0663f8f
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-18 18:52:20 +0200

    agg.conto

--- jssnack1/script.js
+++ jssnack1/script.js
@@ -1,9 +1,10 @@
 
 const numeri=[];
-for(i=0;i<10;i++){
+let conto;
+for(let i=0;i<10;i++){
     let Numero=parseInt(prompt('inserisci un numero!'));
     numeri.push(Numero);
-    let numero=parseInt(numeri[0]);
+    let numero=numeri[0];
     let blocknotes=Numero+numero;
     numeri.push(blocknotes);
     console.log(numeri);
