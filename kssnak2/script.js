88aa2f24a3f5d72fecc68347f7b129de7618cef7
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-18 20:20:48 +0200

    imposto snack2

--- kssnak2/script.js
+++ kssnak2/script.js
@@ -1,5 +1,16 @@
-console.log('ciao');
 
-const invitati=['mario','luigi','francesco','carolina'];
-const nome=prompt('scrivi il tuo nome')
+//costruisco lista invitati,
+//chiedo a utente il suo nome 
+//scorro l'array per confronto nome con lista invitati
+//
+const invitati=['mario','luigi','francesco','carolina','ester'];
+const nome=prompt('scrivi il tuo nome');
+let isName=false;
 
+for(let i=0;i<invitati.length;i++){
+    if (nome==invitati[i]){
+       isName=true;
+       console.log(isName)
+    }
+}
+console.log(isName);
