eb89276b643e4a02084c6bcc734094e59bdd92dc
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-18 17:22:47 +0200

    provo a sommare i num.dentro array

--- jssnack1/script.js
+++ jssnack1/script.js
@@ -3,5 +3,7 @@ const numeri=[];
 for(i=0;i<10;i++){
     let Numero=parseInt(prompt('inserisci un numero!'));
     numeri.push(Numero);
+     Numero=numeri[0] + numeri[1];
+    numeri.push(Numero);
     console.log(numeri);
 }
