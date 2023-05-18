c616b4a54c4f74aa50e1075ccbb9f28c891ac0a9
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-18 15:54:54 +0200

    tentativo snak1

--- jssnack1/script.js
+++ jssnack1/script.js
@@ -1,1 +1,16 @@
-console.log('hello')
+
+//chiedo utente numeri,
+  
+//
+let numeri;
+for(let i=0;i<10;i++){
+ const Number=prompt('inserisci un numero')
+ numero=parseInt(Number.value);
+  numeri=[i];
+ numeri.push(Number);
+ console.log(Number);
+ console.log(numeri);
+ let somma=i+Number;
+console.log(somma)
+}
+ 
