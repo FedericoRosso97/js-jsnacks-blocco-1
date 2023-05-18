e2b490704aeb43dce1c475b7bde92d2e7952b289
FedericoRosso97 <federicorosso97@gmail.com>
2023-05-18 16:31:20 +0200

    fixed

--- jssnack1/script.js
+++ jssnack1/script.js
@@ -1,7 +1,8 @@
-console.log('ciao')
+
+const numeri=[];
 for(i=0;i<10;i++){
     let Numero=prompt('inserisci un numero!');
-    const numeri=[Numero];
+    
     numeri.push(Numero);
     console.log(numeri);
 }
